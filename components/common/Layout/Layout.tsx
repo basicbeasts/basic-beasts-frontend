dfb3b9ed7ddd3ba791dfd168875d0ad3e19d83fd
bz <Bobby@swoopmedia.dk>
2021-10-18 17:42:48 +0200

    trying to push to mainnet

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -6,12 +6,21 @@ import * as fcl from '@onflow/fcl'
 import Head from "next/head"
 
 // Configure FCL for Flow testnet.
+/*
 fcl
   .config()
   .put('accessNode.api', 'https://access-testnet.onflow.org')
   .put('challenge.handshake', 'https://flow-wallet-testnet.blocto.app/authn')
   .put("0xFungibleToken", "0x9a0766d93b6608b7")
   .put("0xFUSD", "0xe223d8a629e49c68")
+*/
+//Configure FCL for Flow mainnet.
+fcl
+  .config()
+  .put('accessNode.api', 'https://access.mainnet.nodes.onflow.org:9000')
+  .put('challenge.handshake', 'https://flow-wallet.blocto.app/authn')
+  .put("0xFungibleToken", "0xf233dcee88fe0abe")
+  .put("0xFUSD", "0x3c5959b568896393")
 
 //<SideNavbar isSideNavbarOpen={isSideNavbarOpen} toggle={toggle} />
 
