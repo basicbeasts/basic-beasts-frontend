888f201476adccb463ca73931f353f541d5947c9
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-11-11 00:05:08 -0500

    feat: get user to sign a message to generate a composite sign (#8)

--- components/user/UserProvider.tsx
+++ components/user/UserProvider.tsx
@@ -1,7 +1,9 @@
-import { FC, createContext, useContext, useState, useEffect } from "react"
+import { FC, createContext, useContext, useEffect } from "react"
+import * as fcl from "@onflow/fcl"
 import useFUSD from "@framework/hooks/use-fusd.hook"
-import useCurrentUser from "@framework/hooks/use-current-user.hook"
 import useUserBeasts from "@framework/hooks/use-user-beasts.hook"
+import useCurrentUser from "framework/hooks/use-current-user.hook"
+import useCookie from "framework/hooks/use-cookie.hook"
 
 export interface State {}
 
@@ -10,13 +12,33 @@ const initialState = {}
 const Context = createContext<State | any>(initialState)
 
 const UserProvider: FC = ({ children }) => {
-  const [user]: any = useCurrentUser()
+  const [user, address]: any = useCurrentUser()
+  const [compositeSignature, setCompositeSignature] = useCookie(
+    "user-composite-signature",
+  )
+
   const { data: balance, getFUSDBalance, purchase, loading } = useFUSD(user)
   const {
     data: userBeasts,
     fetchUserBeasts,
     loadingBeast,
   } = useUserBeasts(user)
+
+  useEffect(() => {
+    if (compositeSignature) return
+    if (address) {
+      const signMessage = async () => {
+        const MSG = Buffer.from(process.env.NEXT_PUBLIC_SIGN_MESSAGE!).toString(
+          "hex",
+        )
+        const signedMessage = await fcl.currentUser().signUserMessage(MSG)
+        if (signedMessage && signedMessage.length > 0) {
+          setCompositeSignature(signedMessage[0])
+        }
+      }
+      signMessage()
+    }
+  }, [address, compositeSignature])
 
   return (
     <Context.Provider
@@ -25,6 +47,7 @@ const UserProvider: FC = ({ children }) => {
         getFUSDBalance,
         purchase,
         loading,
+        compositeSignature,
         userBeasts,
         fetchUserBeasts,
         loadingBeast,
