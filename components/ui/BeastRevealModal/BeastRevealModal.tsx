dce7e37a7091b6e45294b1fb8c96daeabaa5ced7
bz <bz.vanity@gmail.com>
2021-11-11 21:05:58 +0100

    Update BeastRevealModal.tsx

--- components/ui/BeastRevealModal/BeastRevealModal.tsx
+++ components/ui/BeastRevealModal/BeastRevealModal.tsx
@@ -71,7 +71,14 @@ const BeastImg = styled.img`
   width: 750px;
 `
 
-const CloseIcon = styled.img``
+const CloseIcon = styled.div`
+  position: absolute;
+  font-size: 100px;
+  right: 50px;
+  cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
+      14 0,
+    pointer !important;
+`
 
 const dropIn = {
   hidden: { opacity: 0 },
@@ -97,6 +104,7 @@ const BeastRevealModal: FC<FuncProps> = ({ handleClose, text }) => {
       >
         <Wrapper>
           <Bg>
+            <CloseIcon onClick={handleClose}>x</CloseIcon>
             <Content>
               <BeastName variants={dropIn} initial="hidden" animate="visible">
                 Moon
