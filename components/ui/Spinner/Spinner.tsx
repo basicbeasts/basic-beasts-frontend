0eda87f51ad0a9a1f8a15937aad27d9f14265ea1
bz <bz.vanity@gmail.com>
2021-11-13 14:44:17 +0100

    Update Spinner.tsx

--- components/ui/Spinner/Spinner.tsx
+++ components/ui/Spinner/Spinner.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components"
 const Overlay = styled.div`
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: 200%;
   top: 0;
   left: 0;
   background: rgba(0, 0, 0, 0.6);
