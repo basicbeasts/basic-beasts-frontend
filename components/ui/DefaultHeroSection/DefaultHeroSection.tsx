51368b8b3f1436a48e89cb82c2099d4c6faa7821
bz <bz.vanity@gmail.com>
2021-11-30 20:34:12 +0100

    Special messages

--- components/ui/DefaultHeroSection/DefaultHeroSection.tsx
+++ components/ui/DefaultHeroSection/DefaultHeroSection.tsx
@@ -10,8 +10,14 @@ const Container = styled.div`
   align-items: center;
   height: 50vh;
   border: solid 25px #111111;
+
   @media (max-width: 700px) {
     border: solid 15px #111111;
+    border-top: none;
+  }
+  @media (max-width: 1420px) {
+    border: solid 25px #111111;
+    border-top: none;
   }
 `
 
@@ -30,15 +36,24 @@ const Content = styled.div`
 
 const H1 = styled.h1`
   margin: 30px 0 0;
-  line-height: 0.5;
+  line-height: 0.8;
   font-size: 4rem;
   color: #fff;
+  text-align: center;
+  @media (max-width: 700px) {
+    font-size: 3rem;
+  }
+  @media (max-width: 400px) {
+    font-size: 2rem;
+  }
 `
 
 const P = styled.p`
   color: #fff;
   line-height: 1.5;
   font-size: 2.5rem;
+
+  text-align: center;
   @media (max-width: 700px) {
     font-size: 1.5rem;
   }
--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import HeaderDark from "@components/ui/HeaderDark"
 import MyCollection from "@components/ui/MyCollection"
+import DefaultHeroSection from "@components/ui/DefaultHeroSection"
 import BeastList from "@components/ui/BeastList"
 import { useUser } from "@components/user/UserProvider"
 import styled from "styled-components"
@@ -27,6 +28,32 @@ const Collection: NextPage = () => {
             title="My Beast Collection"
             description="See your magnificent Beasts"
           />
+          {user.addr == "0x4065d1f7f1e3388c" ? (
+            <DefaultHeroSection
+              title="You have packs reserved"
+              description="You didn't win the shiny gold draw but your order has been reserved for the next drop. You are guaranteed packs."
+            />
+          ) : (
+            <></>
+          )}
+          {[
+            "0x4742010dbfe107da",
+            "0x01d63aa89238a559",
+            "0x0bbfa46ca43d28a0",
+            "0x0eb72c6284442bf4",
+            "0x4f30c4082e0b8b27",
+            "0x58bbc728329a01b7",
+            "0x5a16175a09403578",
+            "0x6ba1f6ffafcc3a56",
+            "0x7aeb7853c084e2a3",
+          ].includes(user.addr) ? (
+            <DefaultHeroSection
+              title="You unlocked the 'Double Down' achievement"
+              description="You have unlocked the secret 'Double Down' achievement for participating in both drops. You have been rewarded 2x packs!"
+            />
+          ) : (
+            <></>
+          )}
           <MyCollection />
         </>
       ) : (
