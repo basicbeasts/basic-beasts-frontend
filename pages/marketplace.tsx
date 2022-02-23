3309be22c486e63c19aae35259fb1818b9754bee
bz <bz.vanity@gmail.com>
2022-02-23 11:45:25 +0100

    Update marketplace.tsx

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -1,5 +1,12 @@
 import type { NextPage } from "next"
 import HeaderDark from "@components/ui/HeaderDark"
+import styled from "styled-components"
+
+const Spacing = styled.div`
+  @media (min-width: 1100px) {
+    padding: 100px 0;
+  }
+`
 
 const Marketplace: NextPage = () => {
   return (
@@ -8,6 +15,7 @@ const Marketplace: NextPage = () => {
         title="Coming Soon!"
         description="Trade your Beasts on the Marketplace. Receive 5% royalties of all trades from a Beast by becoming its First Owner."
       />
+      <Spacing />
     </div>
   )
 }
