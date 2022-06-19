2756e43f364743f72ee65d7abe08410c0ad6cce7
bz <bz.vanity@gmail.com>
2022-06-19 04:05:26 -0400

    pack tab

--- components/ui/PackTab/PackTab.tsx
+++ components/ui/PackTab/PackTab.tsx
@@ -1,11 +1,13 @@
 import React, { FC, Dispatch, SetStateAction } from "react"
 import styled from "styled-components"
 import ItemTabCard from "../ItemTabCard"
-import poop from "public/fungible_tokens/fungible_tokens_thumbnails/poop_thumbnail_v2.png"
 import sushi from "public/fungible_tokens/fungible_tokens_thumbnails/sushi_thumbnail_v2.png"
-import emptyPotionBottle from "public/fungible_tokens/fungible_tokens_thumbnails/empty_potion_bottle_thumbnail_v2.png"
 
-import star from "public/basic_starLevel.png"
+import StarterImg from "public/packs/pack_pf/starter.png"
+import CursedImg from "public/packs/pack_pf/cursed.png"
+import ShinyImg from "public/packs/pack_pf/shiny.png"
+
+import PackTabCard from "../PackTabCard"
 
 const Wrapper = styled.div`
   padding: 35px 20px 0px;
@@ -39,33 +41,19 @@ const BeastTabCardWrapper = styled.div`
 const ThumbnailDetails = styled.div<ThumbnailDetails>`
   color: #fff;
   background: ${(props) => props.backgroundColor || "white"};
-  display: table;
-  clear: both;
   width: 100%;
-  padding: 0px 10px;
+  padding: 5px 10px 25px;
+  margin-bottom: -20px;
   cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
       14 0,
     pointer !important;
 `
 
-const StarLevel = styled.div`
-  vertical-align: middle;
-`
-
-const StarImg = styled.img`
-  margin-top: 10px;
-  width: 1.2em;
-  float: right;
-  user-drag: none;
-  -webkit-user-drag: none;
-  float: left;
-`
-
 const ThumbnailLabel = styled.div`
   margin: 10px 0;
-  float: right;
-  text-align: right;
-  line-height: 1.3em;
+  text-align: center;
+  line-height: 1em;
+  font-size: 1.8em;
 `
 
 type Props = {
@@ -118,93 +106,78 @@ const PackTab: FC<Props> = ({
         className="grid grid-cols-2 gap-x-5 gap-y-5 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-2 xl:gap-x-7 xl:grid-cols-3 2xl:grid-cols-4"
       >
         <li className="relative">
+          <div>
+            <ThumbnailDetails
+              style={{ borderRadius: "12px 12px 0 0" }}
+              backgroundColor={"#737374"}
+            >
+              <ThumbnailLabel>
+                <div>Starter</div>
+              </ThumbnailLabel>
+            </ThumbnailDetails>
+          </div>
           <div
             style={{
-              borderRadius: "20px 20px 0 0",
+              borderRadius: "12px",
             }}
-            className="group block w-full aspect-w-6 aspect-h-6 bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
+            className="group block w-full aspect-w-6 aspect-h-10 bg-gray-100 overflow-hidden"
           >
-            <ItemTabCard
+            <PackTabCard
               id={"1"}
               className="object-cover group-hover:opacity-90"
-              image={sushi}
+              image={StarterImg}
             />
-          </div>
-          <div>
-            <ThumbnailDetails
-              style={{ borderRadius: "0 0 20px 20px" }}
-              backgroundColor={"#E4A9A2"}
-            >
-              <ThumbnailLabel>
-                <div style={{ fontSize: "2em" }}>20x</div>
-              </ThumbnailLabel>
-            </ThumbnailDetails>
           </div>
         </li>
         <li className="relative">
-          <div
-            style={{
-              borderRadius: "20px 20px 0 0",
-            }}
-            className="group block w-full aspect-w-6 aspect-h-6 bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
-          >
-            <ItemTabCard
-              id={"2"}
-              className="object-cover group-hover:opacity-90"
-              image={emptyPotionBottle}
-            />
-          </div>
           <div>
             <ThumbnailDetails
-              style={{ borderRadius: "0 0 20px 20px" }}
-              backgroundColor={"#396042"}
+              style={{ borderRadius: "12px 12px 0 0" }}
+              backgroundColor={"#751AD0"}
             >
               <ThumbnailLabel>
-                <div style={{ fontSize: "2em" }}>20x</div>
+                <div>Cursed Black</div>
               </ThumbnailLabel>
             </ThumbnailDetails>
           </div>
-        </li>
-        <li className="relative">
           <div
             style={{
-              borderRadius: "20px 20px 0 0",
+              borderRadius: "12px",
             }}
-            className="group block w-full aspect-w-6 aspect-h-6 bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
+            className="group block w-full aspect-w-6 aspect-h-10 bg-gray-100 overflow-hidden"
           >
-            <ItemTabCard
+            <PackTabCard
               id={"3"}
               className="object-cover group-hover:opacity-90"
-              image={poop}
+              image={CursedImg}
             />
           </div>
+        </li>
+        <li className="relative">
           <div>
             <ThumbnailDetails
-              style={{ borderRadius: "0 0 20px 20px" }}
-              backgroundColor={"#604E39"}
+              style={{ borderRadius: "12px 12px 0 0" }}
+              backgroundColor={"#FFDA66"}
             >
               <ThumbnailLabel>
-                <div style={{ fontSize: "2em" }}>20x</div>
+                <div style={{ color: "#A15813" }}>Shiny Gold</div>
               </ThumbnailLabel>
             </ThumbnailDetails>
+          </div>
+          <div
+            style={{
+              borderRadius: "12px",
+            }}
+            className="group block w-full aspect-w-6 aspect-h-10 bg-gray-100 overflow-hidden"
+          >
+            <PackTabCard
+              id={"4"}
+              className="object-cover group-hover:opacity-90"
+              image={ShinyImg}
+            />
           </div>
         </li>
       </ul>
-      {/* Example of loading */}
-      {/* {query.$state.isLoading ? (
-        <Spinner />
-      ) : (
-        <BeastThumbnailList>
-          {beasts.map((beastId, i) => (
-            <BeastThumbnail
-              key={beastId + i}
-              id={beastId}
-              selected={beastId === selectedBeast}
-              onClick={() => selectBeast(beastId)}
-            />
-          ))}
-        </BeastThumbnailList>
-      )} */}
     </Wrapper>
   )
 }
---
+++ components/ui/PackTabCard/PackTabCard.tsx
@@ -0,0 +1,120 @@
+import React, { FC } from "react"
+import styled from "styled-components"
+
+const Container = styled.div<{
+  selected?: boolean
+  tagColor: string
+  bgColor: string
+}>`
+  max-width: inherit;
+  width: 100%;
+  color: #000;
+  cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
+      14 0,
+    pointer !important;
+  padding: 20px;
+  background: ${(props) => props.bgColor || "#E5E8E7"};
+`
+
+const Img = styled.img`
+  user-drag: none;
+  -webkit-user-drag: none;
+  margin-bottom: 15px;
+`
+
+const Wrapper = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`
+
+const ButtonWrapper = styled.div`
+  margin: 15px 0 10px;
+`
+
+const Button = styled.button<{
+  borderColor: string
+  insetBorderColor: string
+  bgColor: string
+  fontColor: string
+}>`
+  text-transform: uppercase;
+  padding: 0 20px 3px;
+  font-size: 1.5vw;
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.fontColor};
+
+  box-shadow: ${(props) =>
+    `-3px 0px 0px 0px ${props.borderColor}, 0px -3px 0px 0px ${props.borderColor}, 0px 3px 0px 0px ${props.borderColor}, 
+    3px 0px 0px 0px ${props.borderColor}, inset -3px -3px ${props.insetBorderColor}`};
+
+  border: none;
+  transition: all 0.1s ease 0s;
+  -moz-transition: all 0.1s ease 0s;
+  -webkit-transition: all 0.1s ease 0s;
+  @media (max-width: 1010px) {
+    font-size: 7vw;
+  }
+  @media (max-width: 1010px) {
+    width: 26vw;
+  }
+  &:active {
+    transition: all 0.1s ease 0s;
+    -moz-transition: all 0.1s ease 0s;
+    -webkit-transition: all 0.1s ease 0s;
+    box-shadow: ${(
+      props,
+    ) => `-3px 0px 0px 0px ${props.borderColor}, 0px -3px 0px 0px ${props.borderColor},
+      0px 3px 0px 0px ${props.borderColor}, 3px 0px 0px 0px ${props.borderColor}, inset 3px 3px ${props.insetBorderColor}`};
+  }
+`
+
+type BeastThumbnailProps = {
+  id: string
+  selected?: boolean
+  onClick?: () => void
+  className: string
+  image: StaticImageData
+}
+
+const PackTabCard: FC<BeastThumbnailProps> = ({
+  id,
+  image,
+  ...props
+}: BeastThumbnailProps) => {
+  return (
+    <Container
+      {...props}
+      tagColor={"#FFD966"}
+      bgColor={id === "1" ? "#E5E8E7" : id === "3" ? "#F9EDF7" : "#FFF4D1"}
+    >
+      <>
+        <Img src={image.src} />
+        <Wrapper>
+          <div>One random 1-star beast</div>
+          <div>Amount: 3</div>
+          <ButtonWrapper>
+            <Button
+              borderColor={
+                id === "1" ? "#111823" : id === "3" ? "#751ad0" : "#a15813"
+              }
+              insetBorderColor={
+                id === "1" ? "#737374" : id === "3" ? "#c746af" : "#f3cb23"
+              }
+              bgColor={
+                id === "1" ? "#ababac" : id === "3" ? "#e3bfff" : "#feff95"
+              }
+              fontColor={
+                id === "1" ? "#fff" : id === "3" ? "#751ad0" : "#a15813"
+              }
+            >
+              Unpack
+            </Button>
+          </ButtonWrapper>
+        </Wrapper>
+      </>
+    </Container>
+  )
+}
+export default PackTabCard
