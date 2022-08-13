40a77ba5cfce05d6b79561064d49de1a52980c95
bz <bz.vanity@gmail.com>
2022-08-13 23:55:10 +0200

    fetching rankings data

---
+++ components/ui/RankingList/RankingList.tsx
@@ -0,0 +1,13 @@
+import { Dispatch, FC, SetStateAction, useEffect, useState } from "react"
+import styled from "styled-components"
+
+const Container = styled.div``
+
+type Props = {}
+
+const RankingList: FC<Props> = ({}) => {
+  useEffect(() => {}, [])
+
+  return <Container></Container>
+}
+export default RankingList
--- pages/rankings.tsx
+++ pages/rankings.tsx
@@ -1,18 +1,72 @@
 import type { NextPage } from "next"
 import HeaderDark from "@components/ui/HeaderDark"
 import styled from "styled-components"
-import InboxItemListTemp from "@components/ui/InboxItemListTemp"
-
-const Spacing = styled.div`
-  @media (min-width: 1100px) {
-    padding: 100px 0;
-  }
-`
+import { useEffect, useState } from "react"
+import { useAuth } from "@components/auth/AuthProvider"
+import { query } from "@onflow/fcl"
 
 const Rankings: NextPage = () => {
+  const [allHunterScores, setAllHunterScores] = useState()
+  const [allBeastsCollected, setAllBeastsCollected] = useState<any>()
+
+  const { user } = useAuth()
+
+  useEffect(() => {
+    if (user?.addr != null) {
+      fetchAllHunterScores()
+      fetchAllBeastsCollected()
+    }
+  }, [user?.addr])
+
+  const fetchAllHunterScores = async () => {
+    try {
+      let res = await query({
+        cadence: `
+        import HunterScore from 0xHunterScore
+
+        pub fun main(): {Address: UInt32} {
+          return HunterScore.getHunterScores()
+        }
+        `,
+      })
+      setAllHunterScores(res)
+      console.log("all hunter scores" + res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const fetchAllBeastsCollected = async () => {
+    try {
+      let res = await query({
+        cadence: `
+        import HunterScore from 0xHunterScore
+
+        pub fun main(): {Address: [UInt64]} {
+          return HunterScore.getAllBeastsCollected()
+        }
+        `,
+      })
+      setAllBeastsCollected(res)
+      console.log("all beast collected" + res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
       <HeaderDark title="Rankings" description="The top Beast Hunters at BB" />
+      <div>Hunter Scores</div>
+      <pre>{JSON.stringify(allHunterScores, null, 2)}</pre>
+      <div>All Beasts Collected</div>
+      <pre>
+        {JSON.stringify(
+          allBeastsCollected["0x805727b65285a84d"].length,
+          null,
+          2,
+        )}
+      </pre>
     </div>
   )
 }
