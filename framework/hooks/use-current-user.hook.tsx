f6dc83eae08e006ae752c67028511f8ef8c68657
bz <bz.vanity@gmail.com>
2022-11-14 01:22:54 +0100

    Update use-current-user.hook.tsx

--- framework/hooks/use-current-user.hook.tsx
+++ framework/hooks/use-current-user.hook.tsx
@@ -5,25 +5,19 @@ import { SIGN_COOKIE_KEY } from "../cookies"
 
 export default function useCurrentUser() {
   const [user, setUser] = useState({ addr: null })
-  const [, , removeCompositeSignature] = useCookie(SIGN_COOKIE_KEY)
+  // const [, , removeCompositeSignature] = useCookie(SIGN_COOKIE_KEY)
 
   const logIn = () => {
     fcl.authenticate()
   }
 
   const logOut = () => {
-    removeCompositeSignature()
+    // removeCompositeSignature()
     fcl.unauthenticate()
   }
 
   useEffect(() => {
-    let cancel = false
-    if (!cancel) {
-      fcl.currentUser().subscribe(setUser)
-    }
-    return () => {
-      cancel = true
-    }
+    fcl.currentUser().subscribe(setUser)
   }, [])
 
   return [user, user?.addr != null, logIn, logOut]
