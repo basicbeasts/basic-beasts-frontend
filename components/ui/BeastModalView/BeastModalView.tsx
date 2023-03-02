521a1469e4d64b261482eb83ab40d4f03e01ad07
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-03-02 15:10:17 +0100

    Update BeastModalView.tsx

--- components/ui/BeastModalView/BeastModalView.tsx
+++ components/ui/BeastModalView/BeastModalView.tsx
@@ -848,7 +848,7 @@ const BeastModalView: FC<Props> = ({
                                             ? "border-rose-500 text-rose-600"
                                             : "border-pink-500 text-pink-600"
                                           : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                                        "w-1/4 py-4 px-1 text-center border-b-2 font-medium text-md",
+                                        "w-1/4 py-4 px-1 text-center border-b-2 font-medium text-md focus:outline-none",
                                       )}
                                       aria-current={
                                         tab.name === filter ? "page" : undefined
